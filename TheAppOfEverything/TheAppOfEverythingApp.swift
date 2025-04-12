ddb3c780f927bd0aec083fd471e312a141ca4e09
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2025-04-12 16:40:29 +1000

    Initialize xcode project

---
+++ TheAppOfEverything/TheAppOfEverythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheAppOfEverythingApp.swift
+//  TheAppOfEverything
+//
+//  Created by Nicholas Clooney on 12/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct TheAppOfEverythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheAppOfEverythingTests/TheAppOfEverythingTests.swift
@@ -0,0 +1,16 @@
+//
+//  TheAppOfEverythingTests.swift
+//  TheAppOfEverythingTests
+//
+//  Created by Nicholas Clooney on 12/4/2025.
+//
+
+import Testing
+
+struct TheAppOfEverythingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
